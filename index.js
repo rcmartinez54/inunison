9f6bd27550961d21245cbee9625e6d4ccb4ff6fd
Raymond Martinez <rcmartinez1854@gmail.com>
2020-10-22 10:42:15 -0700

    added multiple items

--- index.js
+++ index.js
@@ -13,9 +13,55 @@ function fadeImgOnScroll () {
     // $('#main img').delay(5000).fadeIn(1000);
 }
 
+$(".hamburger").on('click', function() {
+		
+    let $nav = $('nav');		
+
+    $(this).toggleClass("open");
+    if ($nav.hasClass('open-menu')) {
+        $nav.removeClass('open-menu');
+    } else {
+        $nav.addClass('open-menu');
+    }
+ });
+
+function fadeNavOnScroll() {
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 30) {
+            $('#header').fadeOut(400);
+        } else {
+            $('#header').fadeIn(400);
+        }
+    });
+}
+
+function smoothScroll() {
+    $('.nav_links').find('a').click(function() {
+        let $href = $(this).attr('href');
+        let $anchor = $('#'+href).offset();
+        $('body').animate({ scrollTop: $anchor.top }, 3000);
+        return false;
+    });
+}
+
+$('a[href^="#"]').on('click', function(event) {
+
+    let target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
 function handler () {
     fadeIn();
     fadeImgOnScroll();
+    fadeNavOnScroll();
+    smoothScroll();
 }
 
 $(handler);
